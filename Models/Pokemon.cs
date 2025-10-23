6cdb52933195c9ba0f7834a102f8af22e757edcd
Dang Nhat Tinh <tingdang6868@gmail.com>
2025-10-23 11:05:15 +0700

    update: class Pokemon

--- Models/Pokemon.cs
+++ Models/Pokemon.cs
@@ -13,25 +13,48 @@ namespace PokemonProject.Models
 {
     public class Pokemon
     {
-        public string Name { get; set; }
-        public PictureBox Image { get; set; }
+        private string _Name;
+        private PictureBox _Image;
+        private double _HP;
+        private Systems _System;
+
         public Skill Skill1 { get; set; }
         public Skill Skill2 { get; set; }
         public Skill Skill3 { get; set; }
         public Skill Skill4 { get; set; }
-        public double HP { get; set; }
-        public Systems System { get; set; }
+
 
         public Pokemon(string name, PictureBox image, double hp, Systems system, Skill skill1, Skill skill2, Skill skill3, Skill skill4)
         {
-            Name = name;
-            Image = image;
-            HP = hp;
-            System = system;
+            _Name = name;
+            _Image = image;
+            _HP = hp;
+            _System = system;
             Skill1 = skill1;
             Skill2 = skill2;
             Skill3 = skill3;
             Skill4 = skill4;
+        }
+
+        public string Name
+        {
+            get { return _Name; }
+            set { _Name = value; }
+        }
+        public PictureBox Image
+        {
+            get { return _Image; }
+            set { _Image = value; }
+        }
+        public double HP
+        {
+            get { return _HP; }
+            set { _HP = value; }
+        }
+        public Systems System
+        {
+            get { return _System; }
+            set { _System = value; }
         }
 
         public void Attack(Pokemon target)
@@ -49,7 +72,6 @@ namespace PokemonProject.Models
             target.HP -= damage;
             if (target.HP < 0) target.HP = 0;
         }
-
 
         public bool IsCounter(Pokemon target)
         {
